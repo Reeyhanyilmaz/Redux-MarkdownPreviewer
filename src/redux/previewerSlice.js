55e5d7d51ca4f8b788b1c1eccf0fab6e1870bce0
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-11 19:24:22 +0300

    add redux folder

---
+++ src/redux/previewerSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const previewerSlice = createSlice({
+    name: "previewer",
+    initialState: {},
+    reducers: {},
+});
+
+export default createSlice.reducer;
