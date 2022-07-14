012827bcb6e15321c24499f2b1e0b33e4a74a6cb
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-14 12:52:59 +0300

    add ReactMarkdown

--- src/components/PreviewArea/index.js
+++ src/components/PreviewArea/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useSelector , useDispatch } from 'react-redux';
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 
 function PreviewArea() {
+  const markdownText = useSelector(state => state.previewer.markdownText);
+  console.log('markdownText :>> ', markdownText);
   return (
     <div className='border-2 basis-1/2'>
-
+      <ReactMarkdown children={markdownText} remarkPlugins={[remarkGfm]}/>
     </div>
   )
 }
