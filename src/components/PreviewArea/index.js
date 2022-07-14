236f5ba7a6597fe588f4020ac053cf8952b1a12d
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-14 12:01:36 +0300

    add componnets

--- src/components/PreviewArea/index.js
+++ src/components/PreviewArea/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function PreviewArea() {
   return (
-    <div className='border-2 basis-1/2'>PreviewArea</div>
+    <div className='border-2 basis-1/2'>
+
+    </div>
   )
 }
 
--- src/components/TextArea/index.js
+++ src/components/TextArea/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useSelector , useDispatch } from 'react-redux';
+import { setMarkdownText } from '../../redux/previewerSlice';
 
 function TextArea() {
+  const dispatch = useDispatch();
+  const markdownText = useSelector(state => state.previewer.markdownText);
+  console.log('markdownText', markdownText);
+
   return (
-    <div className='border-2 basis-1/2'>TextArea</div>
+    <textarea className='border-2 basis-1/2 p-3' value={markdownText} onChange={(e) => dispatch(setMarkdownText(e.target.value))}/>
   )
 }
 
