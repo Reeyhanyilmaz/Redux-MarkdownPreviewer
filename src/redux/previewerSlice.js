f4b254e6f935934b616303dfefc352d1808f7858
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-14 12:01:20 +0300

    add setMarkdownText

--- src/redux/previewerSlice.js
+++ src/redux/previewerSlice.js
@@ -2,8 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const previewerSlice = createSlice({
     name: "previewer",
-    initialState: {},
-    reducers: {},
+    initialState: {
+        markdownText: localStorage.getItem("markdownText") ? JSON.parse(localStorage.getItem("markdownText")) : "",
+    },
+    reducers: {
+        setMarkdownText: (state, action) => {
+            state.markdownText = action.payload;
+            localStorage.setItem("markdownText", JSON.stringify(state.markdownText));
+        },
+    },
 });
 
-export default createSlice.reducer;
+export const { setMarkdownText } = previewerSlice.actions;
+console.log('createSlice.reducer ', createSlice.reducer);
+export default previewerSlice.reducer;
